27f38fa2f417cd15fd382245ffcc2faca3d73e8e
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-26 19:12:14 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -2,10 +2,10 @@ require_relative "./user.rb"
 
 class Teacher < User
 
-  array = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
+def teach 
+  KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
 
-def teach(array)
-  array.sample
+KNOWLEDGE.sample
 end 
 
 end
